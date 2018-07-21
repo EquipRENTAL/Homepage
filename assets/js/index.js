3f8de8b6e60c0d78bccc6bc1b03dec9b4f01f202
Janell Guzman <janellguzman@gmail.com>
2018-07-21 08:39:29 -0700

    Updating descriptions.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,101 +1,53 @@
-const websites = [{
-    name: "Chef's Den",
+const browse = [{
+    name: "Camping Tents",
     imgName: "chefsden.jpg",
-    link: "https://immense-plateau-63304.herokuapp.com/",
-    github: "https://github.com/bagglerock/Chefs-Den",
-    description: "Recipe Finder using the Yummly API.  Login sytem for saving favorites",
-    technologies: "Node.js, Express.js, Handlebars, bcrypt, MySQL, Sequelize, Javascript, jQuery, HTML, CSS"
+    description: "Tent Info",
+    technologies: "Camping"
 },
 {
-    name: "Knockout Fitness",
+    name: "Coolers",
     imgName: "knockout.jpg",
-    link: "http://knockoutfitnessmuaythai.com",
-    github: "https://github.com/bagglerock/knockout-fitness",
-    description: "Website for Knockout Fitness Muay Thai in Brick NJ",
-    technologies: "HTML, CSS, jQuery"
+    description: "Cooler Info",
+    technologies: "Camping"
 },
 {
-    name: "Scrapoogle",
+    name: "Portable Grills",
     imgName: "scrapoogle.jpg",
-    link: "https://agile-coast-42538.herokuapp.com/",
-    github: "https://github.com/bagglerock/Scrappy-Scraper",
-    description: "Application to scrape news article headers from google news and let you make comments about it (under-construction)",
-    technologies: "Node.js, Express.js, Handlebars, MongoDB, Mongoose, Axios, Cheerio, Javascript, jQuery, HTML, CSS"
+    description: "Grill Info",
+    technologies: "Cookware"
 },
 {
-    name: "Freight Systems NJ",
+    name: "Foldable Chairs",
     imgName: "freight-sys.jpg",
-    link: "https://bagglerock.github.io/freight-sys",
-    github: "https://github.com/bagglerock/freight-sys",
-    description: "Website for Freight Systems of NJ.  A Logistics company for your shipping needs.",
-    technologies: "HTML, CSS, jQuery"
-},
-{
-    name: "Hangman",
-    imgName: "hangman.jpg",
-    link: "https://bagglerock.github.io/Hangman-Game",
-    github: "https://github.com/bagglerock/Hangman-Game",
-    description: "Hangman game for the browser.  Musicians are the subject and spotify comes up on correct guess.",
-    technologies: "Javascript, HTML, CSS, Bootstrap"
-},
-{
-    name: "Rock Paper Scissors - Multiplayer",
-    imgName: "rps.jpg",
-    link: "https://bagglerock.github.io/RPS-Multiplayer",
-    github: "https://github.com/bagglerock/RPS-Multiplayer",
-    description: "Multiplayer Rock, Paper, Scissors with chat.",
-    technologies: "Javascript, jQuery, Firebase, HTML, CSS"
-},
-{
-    name: "Trivia Game",
-    imgName: "trivia.jpg",
-    link: "https://bagglerock.github.io/TriviaGame/",
-    github: "https://github.com/bagglerock/TriviaGame",
-    description: "Trivia Game where I couldn't think of any questions, so I just made odd questions and answers",
-    technologies: "Javascript, jQuery, HTML, CSS, Bootstrap"
-},
-{
-    name: "Clicky Game",
-    imgName: "clicky.jpg",
-    link: "https://mysterious-stream-83031.herokuapp.com/",
-    github: "https://github.com/bagglerock/clicky-game",
-    description: "Clicky Game is a small little game much like memory.  Try not to click on the same picture twice and build up your score.",
-    technologies: "MERN"
-},
-{
-    name: "PeachCo Lister 3.0",
-    imgName: "peachco.gif",
-    link: "https://limitless-reaches-23686.herokuapp.com",
-    github: "https://github.com/bagglerock/peachco-react",
-    description: "PeachCo Lister is the listing program for PeachCo that integrates with Ebay's API to list products from a database",
-    technologies: "ReactJS, MySQL, NodeJS, Express, jsftp, node-ebay-api, multer"
+    description: "Chair Info",
+    technologies: "Camping"
 }
 ];
 
 
 
 const logos = [{
-    name: "Smokin 55 BBQ",
+    name: "Camping Gear",
     img: "daves_logo_lighter.png"
 },
 {
-    name: "Island Rides",
+    name: "Cookware",
     img: "jam.png"
 },
 {
-    name: "Material Memories",
+    name: "Tables and Chairs",
     img: "Material Memories.jpg"
 },
 {
-    name: "RHE Gen II",
+    name: "Water Sports",
     img: "rhe.png"
 },
 ];
 
 
-const skills = ["Javascript", "ReactJs" ,"Node.js", "Express.js", "Firebase", "MongoDB", "Ajax", "RESTful", "API's", "PHP", "jQuery", "MySQL", "HTML", "CSS", "Bootstrap", "Adobe Illustrator", "Adobe Photoshop", "Vectornator", "Git"];
+const skills = ["EquipRENT is so AWESOME!", "I get to try things without having to buy things.", "My stuff doesn't have to collect dust anymore. Instead I get to collect money!"];
 
-const imageLocationWebsite = "./assets/img/websites/";
+const imageLocationBrowse = "./assets/img/browse/";
 const imageLocationLogos = "./assets/img/logos/";
 
 
@@ -107,14 +59,12 @@ $(window).scroll(function () {
     $(".headline").css("opacity", 1 - $(window).scrollTop() / 250);
   });
 
-for (let i = 0; i <  websites.length ; i++) {
+for (let i = 0; i <  browse.length ; i++) {
 
-    let name = websites[i].name;
-    let imgName = websites[i].imgName;
-    let link = websites[i].link;
-    let github = websites[i].github;
-    let description = websites[i].description;
-    let technologies = websites[i].technologies;
+    let name = browse[i].name;
+    let imgName = browse[i].imgName;
+    let description = browse[i].description;
+    let technologies = browse[i].technologies;
 
     let mainDiv = $("<div>");
 
@@ -153,7 +103,7 @@ for (let i = 0; i <  websites.length ; i++) {
 
 
     $(mainDiv).append(imgDiv, nameDiv, linksDiv, gitDiv, descriptionDiv, techDiv);
-    $("#websites-area").append(mainDiv);
+    $("#browse-area").append(mainDiv);
 
 }
 
